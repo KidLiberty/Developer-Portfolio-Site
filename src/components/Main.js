52a9c9ac99ef8c334e7baac29c0480cf694eafe7
Brandon Stone <brandonrstone@gmail.com>
2022-02-09 18:36:58 -0500

    Adding parallax effect

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 export default function Main() {
+  const [offsetY, setOffsetY] = useState(0)
+  const handleScroll = () => setOffsetY(window.scrollY)
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll)
+
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [])
+
   return (
-    <div className='main'>I am a Web Developer with a focus on React.js</div>
+    <section className='main'>
+      <div className='main-header'>
+        I am a Web Developer with a focus on React.js
+      </div>
+      <div style={{ transform: `translateY(${offsetY * 0.5}px)` }}>Test</div>
+    </section>
   )
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,27 +1,34 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+
+import Aos from 'aos'
+import 'aos/dist/aos.css'
 
 export default function Projects() {
+  useEffect(() => {
+    Aos.init({ duration: 1000 })
+  }, [])
+
   return (
     <div className='projects-container'>
       <h2 className='projects-header'>Projects</h2>
       <div className='projects-grid'>
-        <div className='grid-item'>
+        <div className='grid-item' data-aos='fade-up'>
           <div className='grid-content'>-Content-</div>
           <div className='grid-content-description'>Coming Soon!</div>
         </div>
-        <div className='grid-item'>
+        <div className='grid-item' data-aos='fade-up'>
           <div className='grid-content'>-Content-</div>
           <div className='grid-content-description'>Coming Soon!</div>
         </div>
-        <div className='grid-item'>
+        <div className='grid-item' data-aos='fade-up'>
           <div className='grid-content'>-Content-</div>
           <div className='grid-content-description'>Coming Soon!</div>
         </div>
-        <div className='grid-item'>
+        <div className='grid-item' data-aos='fade-up'>
           <div className='grid-content'>-Content-</div>
           <div className='grid-content-description'>Coming Soon!</div>
         </div>
-        <div className='grid-item'>
+        <div className='grid-item' data-aos='fade-up'>
           <div className='grid-content'>-Content-</div>
           <div className='grid-content-description'>Coming Soon!</div>
         </div>
