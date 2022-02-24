3bdf18e5cdf192b3fefa97103ada04b069d2e179
Brandon Stone <brandonrstone@gmail.com>
2022-02-23 19:54:39 -0500

    Changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -131,7 +131,7 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <div className='test-container'> Test</div>
+        <div className='test-container'>Test</div>
       </div>
     </>
   )
