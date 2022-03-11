5fc16e542e8612c8e39f9b1a262fb74fae0b03f4
Brandon Stone <brandonrstone@gmail.com>
2022-03-10 22:14:56 -0500

    begun contact information

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 import Navbar from './Navbar'
 
 export default function Contact() {
@@ -7,7 +9,11 @@ export default function Contact() {
     <>
       <Navbar />
       <div className='contact__container'>
-        <div className='contact__email-container'>Test</div>
+        <FontAwesomeIcon icon='fa-brands fa-discord' />
+        <div className='contact__email-container'>
+          The best way to reach be is via email:
+          <h2>brandonrstone@gmail.com</h2>
+        </div>
         <div className='contact__discord-container'>Test</div>
       </div>
     </>
